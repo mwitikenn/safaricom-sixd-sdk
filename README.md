6d6cd32b44e18b9428b67cf8c1f5468d139d6174
Mwiti_Kennedy <ken@africomltd.com>
2020-10-11 20:16:49 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# safaricom-sixd-sdk
---
+++ src/main/java/com/indepthkenya/safaricomsixdsdk/App.java
@@ -0,0 +1,13 @@
+package com.indepthkenya.safaricomsixdsdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
